82e3a34466baafed68ed523b4e68da14f27fb67b
dhchan <daniel.hoyin.chan@gmail.com>
2019-12-10 16:01:16 -0800

    first commit with auth and logging added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const winston = require('winston');
 const { NODE_ENV } = require('./config')
 
 const app = express()
@@ -11,9 +12,36 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
